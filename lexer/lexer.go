c772623262cc44392b1b9a532712a98655dbcbc1
uvaw <akimovivan13@gmail.com>
2025-05-03 15:42:49 +0300

    Fix lexer error for the ==, != cases
    Was byte + byte to string, instead of byte to string
    plus byte to string

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -36,7 +36,7 @@ func (l *Lexer) NextToken() token.Token {
 		if l.peekChar() == '=' {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.EQ, Literal: string(ch + l.ch)}
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
 		} else {
 			tok = newToken(token.ASSIGN, l.ch)
 		}
@@ -60,7 +60,7 @@ func (l *Lexer) NextToken() token.Token {
 		if l.peekChar() == '=' {
 			ch := l.ch
 			l.readChar()
-			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch + l.ch)}
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
 		} else {
 			tok = newToken(token.BANG, l.ch)
 		}
